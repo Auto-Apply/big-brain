7d1c83c3f60cae58f19b034c7b9ba5ccb542743b
remcalu <remcalu@outlook.com>
2023-05-14 21:11:20 -0400

    Fix security issues on main

--- bigbrain/settings.py
+++ bigbrain/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
--- bigbrain/views.py
+++ bigbrain/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def index() -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
